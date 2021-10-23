24b85a347afb1fbca34034a75773bf799979d434
AqingCyan <aqingcyan@cyanthing.com>
2021-10-24 02:49:03 +0800

    add: 猜数游戏

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use rand::Rng; // trait
+use std::cmp::Ordering;
+use std::io; // prelude
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess number game ~");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("Secret number is {}", secret_number);
+    println!("Guess a number！");
+
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Can't read");
+
+    // shadow
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
+
+    println!("Your number is {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("To big"),
+        Ordering::Equal => println!("You win"),
+    }
 }
