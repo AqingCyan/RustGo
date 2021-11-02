8d195855ed2b87a1c970c4795868357941a02744
AqingCyan <aqingcyan@cyanthing.com>
2021-11-02 20:19:46 +0800

    add: 猜数游戏：输入部分

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,14 @@
-use rand::Rng;
-use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess number game ~");
+    println!("Guess the number!");
+    println!("Please input your guess.");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let mut guess = String::new();
 
-    println!("Secret number is {}", secret_number);
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line.");
 
-    loop {
-        println!("Guess a number！");
-
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Can't read");
-
-        // shadow
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("Your number is {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small"),
-            Ordering::Greater => println!("To big"),
-            Ordering::Equal => {
-                println!("You win");
-                break;
-            }
-        }
-    }
+    println!("Your guessed: {}", guess);
 }
